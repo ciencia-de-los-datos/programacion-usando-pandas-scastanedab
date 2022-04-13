a1dd1b42a6819a69b6ba0fe3ea277f84f168a4f9
scastanedab <101910033+scastanedab@users.noreply.github.com>
2022-04-12 21:27:39 -0500

    Update preguntas.py
    Taller 2

--- preguntas.py
+++ preguntas.py
@@ -176,8 +176,8 @@ def pregunta_10():
     4   E  1:1:2:3:3:4:5:5:5:6:7:8:8:9
     """
     dataf = pd.DataFrame({'_c2': (tbl0.groupby('_c1')['_c2'])})
-    datafC1 = pd.DataFrame({'_c1': dataf['_c2'].map(lambda x: x[0])})
-    datafC2 = pd.DataFrame({'_c2': dataf['_c2'].map(lambda x: ':'.join(map(str,sorted(x[1]))))})
+    dataf1 = pd.DataFrame({'_c1': dataf['_c2'].map(lambda x: x[0])})
+    dataf2 = pd.DataFrame({'_c2': dataf['_c2'].map(lambda x: ':'.join(map(str,sorted(x[1]))))})
     data10 = pd.concat([dataf1,dataf2],axis=1)
     data10.set_index('_c1', inplace = True)
     return data10
